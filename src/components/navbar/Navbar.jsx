d5ce0d41526ae232b4e85a1420a44ca4a7ad5964
Timur <timengalytchev@mail.ru>
2023-04-14 04:16:11 +0300

    responsive navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import './navbar.css'
+
+function Navbar()  {
+const [active, setActive] = useState("menu");
+
+
+const togglen = () => {
+  active === 'menu' 
+  ? setActive('menu active') 
+  : setActive('menu');
+
+}
+
+  return (
+    <nav className='nav'>
+        <a className='brand'>Brand</a>
+        <ul className={active}>
+            <li className='item'><a className='link'>Home</a></li>
+            <li className='item'><a className='link'>About</a></li>
+            <li className='item'><a className='link'>Contacts</a></li>
+            <li className='item'><a className='link'>FAQ</a></li>
+        </ul>
+        <div onClick={togglen} className='toggle'>
+            <div className='l1'></div>
+            <div className='l2'></div>
+            <div className='l3'></div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
