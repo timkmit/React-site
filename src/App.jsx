b6d4d272194889b9dbf779ba968a36db1af0dbcd
Timur <timengalytchev@mail.ru>
2023-04-20 15:36:37 +0300

    add 1 modal window

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import UnderHeader from './components/underheader/UnderHeader'
 import RenderCard from './components/body/RenderCard'
+import Modal from './components/modal/Modal'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
+      <Modal />
       <UnderHeader/>
       <RenderCard/>
     </div>
