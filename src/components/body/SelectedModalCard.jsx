a6964c7432f13fb273c88ddec60e585f55d54ff2
Timur <timengalytchev@mail.ru>
2023-04-20 18:01:19 +0300

    add download image

--- src/components/body/SelectedModalCard.jsx
+++ src/components/body/SelectedModalCard.jsx
@@ -4,6 +4,18 @@ import './Cards.css'
 export default function SelectedModalCard(props) {
     const {title, descriptions, img} = props;
 
+    const downloadImage = () => {
+        // создаем ссылку на изображение
+        const link = document.createElement('a');
+        link.download = `${title}.jpg`;
+        link.href = img;
+    
+        // добавляем ссылку на страницу и кликаем по ней
+        document.body.appendChild(link); 
+        link.click(); 
+        document.body.removeChild(link);
+      }
+
     return (
         <div className="wrapper2">
           <div className="main">
@@ -25,6 +37,12 @@ export default function SelectedModalCard(props) {
     
             <h3>Картинка:</h3>
             <p>{img}</p>
+            <button className='downloadButton'>
+            <a className="download" href={img} download={`${title}.jpg`} onClick={downloadImage}>
+            Скачать
+          </a>
+            </button>
+            
           </div>
         </div>
       )
