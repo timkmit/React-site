52fd0cf276e60c53e90d40399c16d40e58de7618
Timur <timengalytchev@mail.ru>
2023-04-20 17:25:32 +0300

    added fix for dymanically render modal w

--- src/components/body/RenderCard.jsx
+++ src/components/body/RenderCard.jsx
@@ -3,59 +3,71 @@ import Cards from './Cards';
 import axios from "axios";
 import './Cards.css'
 
-export default function RenderCard() {
-
+function RenderCard() {
     const [cards, setCards] = useState([]);
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+    const [selectedCard, setSelectedCard] = useState(null);
 
     useEffect(() => {
         axios
-            .get(`http://127.0.0.1:8000/`)
+            .get('http://127.0.0.1:8000/')
             .then(response => {
-                response
-                    .data
-                    .forEach(element => {
-                        setCards(cards => [
-                            ...cards,
-                            element
-                        ]);
-
-                    });
+                setCards(response.data);
+            })
+            .catch(error => {
+                console.error('Ошибка при загрузке данных: ', error);
             });
-
     }, []);
 
-    return (
+    const handleOpenModal = (index) => {
+        setSelectedCard(index);
+        setOpen(true);
+    }
 
+    const handleCloseModal = () => {
+        setSelectedCard(null);
+        setOpen(false);
+    }
+
+    return (
         <div className="list">
             <div className="itemList">
                 {
-                    cards.map((cards, value) => (
-                        <div key={value}>
+                    cards.map((card, index) => (
+                        <div key={index}>
                             <div className="modall">
-                                <button onClick={() => setOpen(true)} className="open-modal-btn"><Cards title={cards.title} img={cards.photo} descriptions={cards.descriptions}/></button>
-                                <div
-                                    className={`overlay animated ${open
-                                        ? 'show'
-                                        : ''}`}>
-                                    <div className="modal">
-                                        <svg
-                                            onClick={() => setOpen(false)}
-                                            height="200"
-                                            viewBox="0 0 200 200"
-                                            width="200">
-                                            <title/>
-                                            <path
-                                                d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
-                                        </svg>
-                                        <Cards title={cards.title} img={cards.photo} descriptions={cards.descriptions}/>
-                                    </div>
-                                </div>
+                                <button onClick={() => handleOpenModal(index)} className="open-modal-btn">
+                                    <Cards title={card.title} img={card.photo} descriptions={card.descriptions}/>
+                                </button>
                             </div>
                         </div>
                     ))
                 }
             </div>
+
+            <div
+                className={`overlay animated ${open
+                    ? 'show'
+                    : ''}`}>
+                <div className="modal">
+                    <svg onClick={handleCloseModal} height="200" viewBox="0 0 200 200" width="200">
+                        <title/>
+                        <path
+                            d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
+                    </svg>
+
+                    {
+                        selectedCard !== null && (
+                            <Cards
+                                title={cards[selectedCard].title}
+                                img={cards[selectedCard].photo}
+                                descriptions={cards[selectedCard].descriptions}
+                                handleCloseModal={handleCloseModal}/>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     )
 }
+export default RenderCard;
